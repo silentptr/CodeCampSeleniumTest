094802940e3c37a4205f60f344b36ed0a40a0455
silent_ptr <imquiet07@gmail.com>
2021-08-24 14:26:38 +1000

    clicks more buttons

--- src/test/java/PlaygroundTestSuite.java
+++ src/test/java/PlaygroundTestSuite.java
@@ -6,13 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class PlaygroundTestSuite
 {
     @Test
-    public void exampleTest()
+    public void exampleTest() throws InterruptedException
     {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://d1iw6mb9di5l9r.cloudfront.net/");
         webDriver.findElement(By.id("forename")).sendKeys("Logan");
         webDriver.findElement(By.id("submit")).click();
+
+        long wait = 5000L;
         webDriver.findElement(By.cssSelector("a[role='button']")).click();
+        Thread.sleep(wait);
+        webDriver.findElement(By.cssSelector("button[aria-label='login']")).click();
+        Thread.sleep(wait);
+        webDriver.findElement(By.cssSelector("button[aria-label='cancel']")).click();
+        Thread.sleep(wait);
         webDriver.findElement(By.cssSelector("button[aria-label='home']")).click();
+        Thread.sleep(wait);
+        webDriver.findElement(By.cssSelector("a[aria-label='link-button']")).click();
     }
 }
