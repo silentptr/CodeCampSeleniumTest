1600cc81bd6550f7edb5d50c0a42f5ed3625dc60
silent_ptr <imquiet07@gmail.com>
2021-08-24 15:20:34 +1000

    even more button cicking

--- src/test/java/PlaygroundTestSuite.java
+++ src/test/java/PlaygroundTestSuite.java
@@ -1,27 +1,43 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class PlaygroundTestSuite
 {
-    @Test
-    public void exampleTest() throws InterruptedException
+    WebDriver webDriver;
+
+    @BeforeEach
+    public void beforeExampleTest()
     {
-        WebDriver webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver();
         webDriver.get("https://d1iw6mb9di5l9r.cloudfront.net/");
+        webDriver.manage().window().maximize();
+    }
+
+    @Test
+    public void exampleTest()
+    {
         webDriver.findElement(By.id("forename")).sendKeys("Logan");
         webDriver.findElement(By.id("submit")).click();
+    }
 
-        long wait = 5000L;
-        webDriver.findElement(By.cssSelector("a[role='button']")).click();
-        Thread.sleep(wait);
+    @Test
+    public void webDriverWaitTest()
+    {
         webDriver.findElement(By.cssSelector("button[aria-label='login']")).click();
-        Thread.sleep(wait);
-        webDriver.findElement(By.cssSelector("button[aria-label='cancel']")).click();
-        Thread.sleep(wait);
-        webDriver.findElement(By.cssSelector("button[aria-label='home']")).click();
-        Thread.sleep(wait);
-        webDriver.findElement(By.cssSelector("a[aria-label='link-button']")).click();
+        new WebDriverWait(webDriver, 10000L).until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-message")));
+        webDriver.findElement(By.cssSelector("a[aria-label='users']")).click();
+    }
+
+    @AfterEach
+    public void afterExampleTest()
+    {
+        webDriver.quit();
     }
 }
