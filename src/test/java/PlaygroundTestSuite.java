cdb73dcd819d12c0e78b827b802d8dc008fb9156
silent_ptr <imquiet07@gmail.com>
2021-08-25 10:38:34 +1000

    modern form test

--- src/test/java/PlaygroundTestSuite.java
+++ src/test/java/PlaygroundTestSuite.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Calendar;
@@ -52,6 +53,28 @@ public class PlaygroundTestSuite
         webDriver.findElement(By.className("v-card__actions")).findElement(By.cssSelector("button[id='loginButton']")).click();
         new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("v-messages__message")));
         Assertions.assertEquals(webDriver.findElement(By.className("v-messages__message")).getText(), "Invalid user and password");
+    }
+
+    @Test
+    public void formsModernErrorTest()
+    {
+        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='#/forms']")));
+        webDriver.findElement(By.cssSelector("a[href='#/forms']")).click();
+        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
+        webDriver.findElement(By.cssSelector("input[name='name']")).sendKeys("a");
+        webDriver.findElement(By.cssSelector("input[name='email']")).sendKeys("a@a.aa");
+        webDriver.findElement(By.className("v-select__selections")).click();
+        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[role='listbox']")));
+        webDriver.findElement(By.cssSelector("div[role='listbox']")).findElements(By.cssSelector("div[role='option']")).forEach(option ->
+        {
+            if (option.findElement(By.className("v-list-item__content")).findElement(By.className("v-list-item__title")).getText().equals("QLD"))
+            {
+                option.click();
+            }
+        });
+        webDriver.findElement(By.className("v-input--selection-controls__ripple")).click();
+        webDriver.findElement(By.className("v-card__text")).findElement(By.tagName("button")).click();
+        new WebDriverWait(webDriver, 20L).until(ExpectedConditions.visibilityOfElementLocated(By.className("popup-message")));
     }
 
     @AfterEach
