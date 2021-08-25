8ba892dd89c5e9707800364dadee1bfee03d741e
silent_ptr <imquiet07@gmail.com>
2021-08-25 13:45:46 +1000

    test model class

---
+++ src/test/java/HomePage.java
@@ -0,0 +1,144 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class HomePage
+{
+    private WebDriver _webDriver;
+    private String date;
+
+    public HomePage(WebDriver webDriver)
+    {
+        _webDriver = webDriver;
+        Calendar c = Calendar.getInstance(TimeZone.getDefault());
+        date = String.format("%d%02d%02d", c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH));
+    }
+
+    public HomePage clickLoginButton()
+    {
+        _webDriver.findElement(By.className("v-card__actions")).findElement(By.cssSelector("button[id='loginButton']")).click();
+        return this;
+    }
+
+    public HomePage clickFakeLoginButton()
+    {
+        _webDriver.findElement(By.cssSelector("button[aria-label='login']")).click();
+        return this;
+    }
+
+    public HomePage clickUserButton()
+    {
+        _webDriver.findElement(By.cssSelector("a[aria-label='users']")).click();
+        return this;
+    }
+
+    public HomePage clickForenameSubmitButton()
+    {
+        _webDriver.findElement(By.id("submit")).click();
+        return this;
+    }
+
+    public HomePage clickAgreeCheckbox()
+    {
+        _webDriver.findElement(By.className("v-input--selection-controls__ripple")).click();
+        return this;
+    }
+
+    public HomePage clickFormSubmitButton()
+    {
+        _webDriver.findElement(By.className("v-card__text")).findElement(By.tagName("button")).click();
+        return this;
+    }
+
+    public boolean selectStateDropdown(String selection)
+    {
+        boolean validSelection = false;
+        _webDriver.findElement(By.className("v-select__selections")).click();
+        WebElement listBox = _webDriver.findElement(By.cssSelector("div[role='listbox']"));
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.visibilityOf(listBox));
+
+        for (WebElement option : listBox.findElements(By.cssSelector("div[role='option']")))
+        {
+            if (option.findElement(By.className("v-list-item__content")).findElement(By.className("v-list-item__title")).getText().equals(selection))
+            {
+                option.click();
+                validSelection = true;
+            }
+        }
+
+        return validSelection;
+    }
+
+    public HomePage waitForLoginMessage()
+    {
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("v-messages__message")));
+        return this;
+    }
+
+    public HomePage waitForLoginDialog()
+    {
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("input[name='gen-" + date + "-username']")));
+        return this;
+    }
+
+    public HomePage waitForFakeLoginMessage()
+    {
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-message")));
+        return this;
+    }
+
+    public HomePage waitForFormMessage()
+    {
+        new WebDriverWait(_webDriver, 20L).until(ExpectedConditions.visibilityOfElementLocated(By.className("popup-message")));
+        return this;
+    }
+
+    public String getLoginMessage()
+    {
+        return _webDriver.findElement(By.className("v-messages__message")).getText();
+    }
+
+    public HomePage sendLoginUserKeys(CharSequence keys)
+    {
+        _webDriver.findElement(By.cssSelector("input[name='gen-" + date + "-username']")).sendKeys(keys);
+        return this;
+    }
+
+    public HomePage sendLoginPasswordKeys(CharSequence keys)
+    {
+        _webDriver.findElement(By.cssSelector("input[name='gen-" + date + "-password']")).sendKeys(keys);
+        return this;
+    }
+
+    public HomePage sendForenameKeys(CharSequence keys)
+    {
+        _webDriver.findElement(By.id("forename")).sendKeys(keys);
+        return this;
+    }
+
+    public HomePage sendFormNameKeys(CharSequence keys)
+    {
+        _webDriver.findElement(By.cssSelector("input[name='name']")).sendKeys(keys);
+        return this;
+    }
+
+    public HomePage sendFormEmailKeys(CharSequence keys)
+    {
+        _webDriver.findElement(By.cssSelector("input[name='email']")).sendKeys(keys);
+        return this;
+    }
+
+    public HomePage clickFormsButton()
+    {
+        WebElement formsLink = _webDriver.findElement(By.cssSelector("a[href='#/forms']"));
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.elementToBeClickable(formsLink));
+        formsLink.click();
+        new WebDriverWait(_webDriver, 2L).until(ExpectedConditions.visibilityOf(_webDriver.findElement(By.cssSelector("input[name='name']"))));
+        return this;
+    }
+}
--- src/test/java/PlaygroundTestSuite.java
+++ src/test/java/PlaygroundTestSuite.java
@@ -1,20 +1,11 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.Calendar;
-import java.util.TimeZone;
 
 public class PlaygroundTestSuite
 {
     WebDriver webDriver;
+    HomePage homePage;
 
     @BeforeEach
     public void beforeExampleTest()
@@ -22,59 +13,41 @@ public class PlaygroundTestSuite
         webDriver = new ChromeDriver();
         webDriver.get("https://d1iw6mb9di5l9r.cloudfront.net/");
         webDriver.manage().window().maximize();
+        homePage = new HomePage(webDriver);
     }
 
     @Test
     public void exampleTest()
     {
-        webDriver.findElement(By.id("forename")).sendKeys("Logan");
-        webDriver.findElement(By.id("submit")).click();
+        homePage.sendForenameKeys("Logan").clickForenameSubmitButton();
     }
 
     @Test
     public void webDriverWaitTest()
     {
-        webDriver.findElement(By.cssSelector("button[aria-label='login']")).click();
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-message")));
+        homePage.clickFakeLoginButton().waitForFakeLoginMessage();
     }
 
     @Test
-    public void userLoginErrorTest()
+    public void userLoginTest()
     {
-        Calendar c = Calendar.getInstance(TimeZone.getDefault());
-        String date = String.format("%d%02d%02d", c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH));
-        webDriver.findElement(By.cssSelector("a[aria-label='users']")).click();
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='gen-" + date + "-username']")));
-        webDriver.findElement(By.cssSelector("input[name='gen-" + date + "-username']")).sendKeys("test_username");
-        webDriver.findElement(By.cssSelector("input[name='gen-" + date + "-password']")).sendKeys("test_password");
-        webDriver.findElement(By.className("v-card__actions")).findElement(By.cssSelector("button[id='loginButton']")).click();
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("v-messages__message")));
-        Assertions.assertEquals(webDriver.findElement(By.className("v-messages__message")).getText(), "Invalid user and password");
+        homePage.clickUserButton().waitForLoginDialog();
+        homePage.sendLoginUserKeys("username").sendLoginPasswordKeys("password");
+        homePage.clickLoginButton().waitForLoginMessage();
+        Assertions.assertEquals(homePage.getLoginMessage(), "Invalid user and password");
     }
 
     @Test
     public void formsModernErrorTest()
     {
-        WebElement formsLink = webDriver.findElement(By.cssSelector("a[href='#/forms']"));
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.elementToBeClickable(formsLink));
-        formsLink.click();
-        WebElement nameField = webDriver.findElement(By.cssSelector("input[name='name']"));
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOf(nameField));
-        nameField.sendKeys("a");
-        webDriver.findElement(By.cssSelector("input[name='email']")).sendKeys("a@a.aa");
-        webDriver.findElement(By.className("v-select__selections")).click();
-        WebElement listBox = webDriver.findElement(By.cssSelector("div[role='listbox']"));
-        new WebDriverWait(webDriver, 2L).until(ExpectedConditions.visibilityOf(listBox));
-        listBox.findElements(By.cssSelector("div[role='option']")).forEach(option ->
-        {
-            if (option.findElement(By.className("v-list-item__content")).findElement(By.className("v-list-item__title")).getText().equals("QLD"))
-            {
-                option.click();
-            }
-        });
-        webDriver.findElement(By.className("v-input--selection-controls__ripple")).click();
-        webDriver.findElement(By.className("v-card__text")).findElement(By.tagName("button")).click();
-        new WebDriverWait(webDriver, 20L).until(ExpectedConditions.visibilityOfElementLocated(By.className("popup-message")));
+        homePage.clickFormsButton();
+        homePage.sendFormNameKeys("a")
+                .sendFormEmailKeys("a@a.aa");
+
+        homePage.clickAgreeCheckbox();
+        homePage.selectStateDropdown("QLD");
+
+        homePage.clickFormSubmitButton().waitForFormMessage();
     }
 
     @AfterEach
