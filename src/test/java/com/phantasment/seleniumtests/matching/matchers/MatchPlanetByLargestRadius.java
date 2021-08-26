e55c1fa46aedf5e70b0b6037d28fd59c63c0e79e
silent_ptr <imquiet07@gmail.com>
2021-08-26 11:13:26 +1000

    more matchers

---
+++ src/test/java/com/phantasment/seleniumtests/matching/matchers/MatchPlanetByLargestRadius.java
@@ -0,0 +1,23 @@
+package com.phantasment.seleniumtests.matching.matchers;
+
+import com.phantasment.seleniumtests.Planet;
+import com.phantasment.seleniumtests.matching.Matchable;
+
+public class MatchPlanetByLargestRadius implements Matchable<Planet>
+{
+    private Planet p;
+
+    @Override
+    public boolean match(Planet planet)
+    {
+        if (p == null && planet.getRadius() > p.getRadius())
+        {
+            p = planet;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
